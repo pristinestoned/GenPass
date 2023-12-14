1cde312619faba243e42d20f14c6635fcb1f894c
pristinestoned <37197210+pristinestoned@users.noreply.github.com>
2023-12-14 15:53:37 -0500

    Add files via upload
    Auto Compile File

--- autocompile.sh
+++ autocompile.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
+if [ -f /usr/bin/cmake ]; then
 echo Running CMake
 cmake ./
+else
+echo You need install CMake
+echo Example: sudo apt install cmake
+fi
+if [ -f /usr/bin/make ]; then
 echo Running Makefile
 make
+else
+echo You need to install Make
+echo Example: sudo apt install make
+fi
